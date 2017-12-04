fed64bcd9064457e239aaa7024829e83196dcc39
Gabriel Silva Tosetti <gabriel.tosetti@hotmail.com>
2017-12-04 11:31:00 -0200

    Aula 5

---
+++ index.js
@@ -0,0 +1,25 @@
+class Message {
+   constructor(text = '', created = Date.now()){
+      this.text = text;
+      this.created = created;
+   }
+   get created(){
+      return this._created;
+   }
+   set created(created){
+      if (!created || isNaN(created)) {
+         throw new Error('Invalid created');
+      }
+      this._created = created;
+   }
+
+   toString(){
+      return `Message created at: ${this.created} - Text: ${this.text}`;
+   }
+}
+
+var emptyMessage = new Message();
+var textMessage   = new Message('Yesterday message', Date.now() - 86400);
+
+console.log(String(emptyMessage));
+console.log(String(textMessage));
