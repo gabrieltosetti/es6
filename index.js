09aecf0427a2e71cb9dfd1d0478a021b8ec6a642
Gabriel Silva Tosetti <gabriel.tosetti@hotmail.com>
2017-12-04 16:29:02 -0200

    Aula 9

---
+++ index.js
@@ -0,0 +1,42 @@
+const Message = require('./message.model');
+const ImageMessage = require('./image-message.model');
+const fs = require('fs');
+const path = require('path');
+
+const emptyMessage = new Message();
+const emptyPhotoMessage = new ImageMessage();
+
+class MessageService {
+  constructor() {
+    let resolvePromise;
+    let rejectPromise;
+    const filePath = path.join(__dirname, 'messages.json');
+    this.messagesPromise = new Promise((resolve, reject) => {
+      resolvePromise = resolve;
+      rejectPromise = reject;
+    });
+    fs.readFile(filePath, { encoding: 'utf-8' }, (err, data) => {
+      if (err) {
+        rejectPromise(err);
+      } else {
+        const dataArray = JSON.parse(data);
+        const dataObj = dataArray.map((item) => {
+          return new Message(item.text, item.created);
+        });
+        resolvePromise(dataObj);
+      }
+    });
+  }
+  get messages() {
+    return this.messagesPromise;
+  }
+}
+
+const messagesService = new MessageService();
+messagesService.messages.then((messages) => {
+  for (let x = 0; x < messages.length; x++) {
+    console.log(String(messages[x]));
+  }
+}).catch(() => {
+  console.log(err);
+});
